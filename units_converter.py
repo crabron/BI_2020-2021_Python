dcf726eff0ee3eb4bed93f8a3216c9c108435ebc
grgladkov <ruginodis@gmail.com>
2020-09-23 23:57:14 +0300

    flake8 - 1

--- units_converter.py
+++ units_converter.py
@@ -5,7 +5,6 @@ df = df.rename(columns={'conversion_type': 'Conversion_type',
                         'from': 'From',
                         'to': 'To',
                         'multiply_by': 'Multiply_by'}, inplace=False)
-
 solitude_type = [i for i in df.Conversion_type.unique()]
 solitude_type_num = [i for i in range(1, len(solitude_type) + 1)]
 solitude_type = zip(solitude_type_num, solitude_type)
@@ -15,14 +14,16 @@ for i in d_solitude_type.keys():
 print("Категории конвертации. Введите число соответствующее нужной категории.")
 sel_1 = int(input())
 type_get = d_solitude_type.get(sel_1)
-solitude_from = [i for i in df[df["Conversion_type"] == type_get].From.unique()]
+solitude_from = [i for i in
+                 df[df["Conversion_type"] == type_get].From.unique()]
 solitude_from_num = [i for i in range(1, len(solitude_from) + 1)]
 solitude_from = zip(solitude_from_num, solitude_from)
 d_solitude_from = dict(solitude_from)
 for i in d_solitude_from.keys():
     print(i, d_solitude_from.get(i))
 print("В данной категории возможна конвертация следующих штуковин. "
-      "Введите число соответствующее нужной штуковине, из которой Вы хотите конвертировать.")
+      "Введите число соответствующее нужной штуковине,"
+      " из которой Вы хотите конвертировать.")
 [print(i[0], i[1]) for i in solitude_from]
 sel_2 = int(input())
 print("В какую штуковину из списка выше вы хотите конвертировать?")
